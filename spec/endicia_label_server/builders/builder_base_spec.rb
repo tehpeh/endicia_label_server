b4251f092e5014e2917e030e5c2ca6988150132b
Paul Trippett <paul@trippett.org>
2015-05-20 16:18:13 +0100

    Add tests for Builderbase.add_hash

--- spec/endicia_label_server/builders/builder_base_spec.rb
+++ spec/endicia_label_server/builders/builder_base_spec.rb
@@ -14,4 +14,18 @@ describe EndiciaLabelServer::Builders::BuilderBase do
       should eql '<MyBuilder><Test><TestID>1</TestID><TestID>2</TestID></Test></MyBuilder>'
     end
   end
+
+  describe '.add_hash' do
+    let(:builder) { EndiciaLabelServer::Builders::BuilderBase.new 'MyBuilder' }
+
+    subject { builder.to_xml.gsub(/\s+/, "") }
+
+    before do
+      builder.add_hash :test, { id: '1', source: 'test' }
+    end
+
+    it "should return valid XML" do
+      should eql '<MyBuilder><Test><ID>1</ID><Source>test</Source></Test></MyBuilder>'
+    end
+  end
 end
